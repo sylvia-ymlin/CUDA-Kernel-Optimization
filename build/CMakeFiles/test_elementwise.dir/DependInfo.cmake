4f3c26ab93c980c9f2aae2b72645ec78c0280046
sylvia-ymlin <gm.ymlin@gmail.com>
2025-12-22 17:07:24 +0000

    perf(build): build the project and add the nsys report

---
+++ build/CMakeFiles/test_elementwise.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gm_ymlin/CUDA-Kernel-Optimization-Playground/src/elementwise/activations.cu" "CMakeFiles/test_elementwise.dir/src/elementwise/activations.cu.o" "gcc" "CMakeFiles/test_elementwise.dir/src/elementwise/activations.cu.o.d"
+  "/home/gm_ymlin/CUDA-Kernel-Optimization-Playground/src/elementwise/elementwise_add.cu" "CMakeFiles/test_elementwise.dir/src/elementwise/elementwise_add.cu.o" "gcc" "CMakeFiles/test_elementwise.dir/src/elementwise/elementwise_add.cu.o.d"
+  "/home/gm_ymlin/CUDA-Kernel-Optimization-Playground/tests/test_elementwise.cu" "CMakeFiles/test_elementwise.dir/tests/test_elementwise.cu.o" "gcc" "CMakeFiles/test_elementwise.dir/tests/test_elementwise.cu.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test_elementwise.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_elementwise.dir/cmake_device_link.o"
+  "CMakeFiles/test_elementwise.dir/src/elementwise/activations.cu.o"
+  "CMakeFiles/test_elementwise.dir/src/elementwise/activations.cu.o.d"
+  "CMakeFiles/test_elementwise.dir/src/elementwise/elementwise_add.cu.o"
+  "CMakeFiles/test_elementwise.dir/src/elementwise/elementwise_add.cu.o.d"
+  "CMakeFiles/test_elementwise.dir/tests/test_elementwise.cu.o"
+  "CMakeFiles/test_elementwise.dir/tests/test_elementwise.cu.o.d"
+  "test_elementwise"
+  "test_elementwise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/test_elementwise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
