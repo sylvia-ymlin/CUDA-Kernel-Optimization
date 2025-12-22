752a22794a500af8243d3a2503dda2336a1fb87d
sylvia-ymlin <gm.ymlin@gmail.com>
2025-12-22 19:57:50 +0000

    docs: pass reduce_sum tests

---
+++ build/CMakeFiles/test_reduction.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_reduction.dir/cmake_device_link.o"
+  "CMakeFiles/test_reduction.dir/src/reduce/reduce_max.cu.o"
+  "CMakeFiles/test_reduction.dir/src/reduce/reduce_max.cu.o.d"
+  "CMakeFiles/test_reduction.dir/src/reduce/reduce_sum.cu.o"
+  "CMakeFiles/test_reduction.dir/src/reduce/reduce_sum.cu.o.d"
+  "CMakeFiles/test_reduction.dir/tests/test_reduction.cu.o"
+  "CMakeFiles/test_reduction.dir/tests/test_reduction.cu.o.d"
+  "test_reduction"
+  "test_reduction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/test_reduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
